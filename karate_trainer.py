696bef943b319e2189ac23c6e13aabd7ccf40a0a
Kevin Tandavo <riyuze211001@gmail.com>
2023-03-07 14:26:39 +0700

    changed to modern widgets

--- karate_trainer.py
+++ karate_trainer.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 import sv_ttk
 
@@ -11,10 +12,10 @@ def main():
     root.geometry("1080x720")
     root.iconbitmap("./assets/karate_trainer.ico")
 
-    title_lbl = tk.Label(root, text="Karate Trainer", font=("Arial Bold", 30))
+    title_lbl = ttk.Label(root, text="Karate Trainer", font=("Arial Bold", 30))
     title_lbl.pack()
 
-    start_btn = tk.Button(root, text="Start", width=40, command=start)
+    start_btn = ttk.Button(root, text="Start", width=40, command=start)
     start_btn.pack()
 
     sv_ttk.set_theme("dark")
