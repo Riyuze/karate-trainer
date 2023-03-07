fcb68fee079005933bc66e0365c827a7a24edba3
Kevin Tandavo <riyuze211001@gmail.com>
2023-03-07 16:02:51 +0700

    Started making frames for multiple windows

--- karate_trainer.py
+++ karate_trainer.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 
-import sv_ttk
+import sv_ttk   
+    
 
 class App(tk.Tk):
     def __init__(self):
@@ -11,18 +12,15 @@ class App(tk.Tk):
         self.title("Karate Trainer")
         self.iconbitmap("./assets/karate_trainer.ico")
 
-        self.create_widgets()
-    
+        sv_ttk.set_theme("dark")
+
+        self.title_lbl = ttk.Label(self, text="Karate Trainer", font=("Times new roman", 30, "bold")).pack(pady=5)
+
+        self.start_btn = ttk.Button(self, text="Start", width=40).pack(pady=5)
+        self.quit_btn = ttk.Button(self, text="Quit", width=40, command=self.quit).pack(pady=5)
+
     def close(self):
         win.quit()
-    
-    def create_widgets(self):
-        title_lbl = ttk.Label(self, text="Karate Trainer", font=("Times new roman", 30, "bold")).pack(pady=5)
-
-        start_btn = ttk.Button(self, text="Start", width=40).pack(pady=5)
-        quit_btn = ttk.Button(self, text="Quit", width=40, command=self.quit).pack(pady=5)
-
-        sv_ttk.set_theme("dark")
 
 if __name__ == "__main__":
     try:  
