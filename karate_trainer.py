d7bb1177bd7957f990ba39b718c8a2b15a5ec67e
Kevin Tandavo <riyuze211001@gmail.com>
2023-03-08 11:48:13 +0700

    Added disable feature to button if training not picked

--- karate_trainer.py
+++ karate_trainer.py
@@ -8,11 +8,17 @@ class Menu(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
-        self.title_lbl = ttk.Label(self, text="Karate Trainer", font=("Times new roman", 30, "bold")).pack(pady=5)
+        self.title_lbl = ttk.Label(self, text="Karate Trainer", font=("Times new roman", 30, "bold"))
+        self.title_lbl.pack(pady=5)
 
-        self.start_btn = ttk.Button(self, text="Start", width=40, command=lambda: controller.show_frame(Choice)).pack(pady=5)
-        self.history_btn = ttk.Button(self, text="History", width=40, command= lambda: controller.show_frame(History)).pack(pady=5)
-        self.quit_btn = ttk.Button(self, text="Quit", width=40, command=self.quit).pack(pady=5)
+        self.start_btn = ttk.Button(self, text="Start", width=40, command=lambda: controller.show_frame(Choice))
+        self.start_btn.pack(pady=5)
+
+        self.history_btn = ttk.Button(self, text="History", width=40, command= lambda: controller.show_frame(History))
+        self.history_btn.pack(pady=5)
+
+        self.quit_btn = ttk.Button(self, text="Quit", width=40, command=self.quit)
+        self.quit_btn.pack(pady=5)
 
     def close(self):
         win.quit()
@@ -28,16 +34,29 @@ class Choice(tk.Frame):
 
         self.option_var = tk.StringVar(self)
 
-        self.title_lbl = ttk.Label(self, text="Pick Your Training Plan", font=("Times new roman", 30, "bold")).pack(pady=5)
+        self.title_lbl = ttk.Label(self, text="Pick Your Training Plan", font=("Times new roman", 30, "bold"))
+        self.title_lbl.pack(pady=5)
 
-        self.option_menu = ttk.OptionMenu(self, self.option_var, "Select one...", *self.OPTIONS)
+        self.option_menu = ttk.OptionMenu(self, self.option_var, "Select one...", *self.OPTIONS, command= lambda _: self.button_state())
         self.option_menu.config(width=38)
         self.option_menu.pack(pady=5)
 
-        self.start_btn = ttk.Button(self, text="Start", width=40, command= lambda: self.start(self.option_var.get())).pack(pady=5)
-        self.back_btn = ttk.Button(self, text="Back", width=40, command= lambda: controller.show_frame(Menu)).pack(pady=5)
+        self.start_btn = ttk.Button(self, text="Start", width=40, command= lambda: self.start())
+        self.start_btn.state(["disabled"])
+        self.start_btn.pack(pady=5)
+        
+        self.back_btn = ttk.Button(self, text="Back", width=40, command= lambda: controller.show_frame(Menu))
+        self.back_btn.pack(pady=5)
 
-    def start(self, train):
+    def button_state(self):
+        train = self.option_var.get()
+        if train != "Select one...":
+            self.start_btn.state(["!disabled"])
+        else:
+            self.start_btn.state(["disabled"])
+
+    def start(self):
+        train = self.option_var.get()
         print(f"Train: {train}")
 
 
@@ -45,9 +64,11 @@ class History(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         
-        self.title_lbl = ttk.Label(self, text="History", font=("Times new roman", 30, "bold")).pack(pady=5)
+        self.title_lbl = ttk.Label(self, text="History", font=("Times new roman", 30, "bold"))
+        self.title_lbl.pack(pady=5)
 
-        self.back_btn = ttk.Button(self, text="Back", width=40, command=lambda: controller.show_frame(Menu)).pack(pady=5)
+        self.back_btn = ttk.Button(self, text="Back", width=40, command=lambda: controller.show_frame(Menu))
+        self.back_btn.pack(pady=5)
     
 
 class App(tk.Tk):
